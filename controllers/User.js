cc6400d8a4844154f656b0635e64e8dfac60f1bb
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-02 23:47:24 +0300

    ft:still_working_on_it:backend

--- controllers/User.js
+++ controllers/User.js
@@ -20,3 +20,34 @@ exports.getUser = async (req,res) => {
         data:users
     })
 }
+exports.usD =  async (req,res) => {
+    const { sessionId,serviceCode,phoneNumber,text } = req.body
+    console.log(req.body)
+    const textValue = text.split('*').length
+    // const message = ""
+    // welcomeMsg = 'Hi, what do you want to do leo?'
+    const orderDetails = {
+        name:"",
+        amount:"",
+        tel:""
+    }
+    const lastData =""
+    if(text === ''){
+        message = 'CON Hi, what do you want to do leo?'
+        
+    } else if(textValue === 1){
+        message = "CON What do you want to eat?"
+        orderDetails.name = text.split('*')[1]
+    } else if (textValue == 2){
+        message = "CON What amount do you want?"
+        orderDetails.amount = text.split('*')[3]
+    } else {
+        const { name,amount} = orderDetails
+        console.log(orderDetails)
+        message = `END Thanks for your order, Enjoy your meals. You have ordered ${name} of ${amount} quantity`
+    }
+    res.contentType('text/plain')
+    res.status(200).send(message)
+    
+    // console.log(req.body)
+}
--- routes/User.js
+++ routes/User.js
@@ -1,10 +1,10 @@
 const User = require('../models/User')
-const { postUser,getUser } = require('../controllers/User')
+const { postUser,getUser,usD } = require('../controllers/User')
 const tryCatchs = require('../middlewares/tryCatch')
 const express = require('express')
 
 const router = express.Router()
 
 router.route('/').get(tryCatchs(getUser)).post(tryCatchs(postUser))
-
+router.route('/ussd/').post(tryCatchs(usD))
 module.exports = router
