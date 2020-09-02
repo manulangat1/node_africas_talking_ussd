0c692d22e841e1b9cf5e17e0395f88b2847fd4b2
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-02 22:14:47 +0300

    ft:successfully_sent_sms:backend

--- controllers/User.js
+++ controllers/User.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 const User = require('../models/User')
-
+const { sendSms} =  require('../utils/send_sms')
 
 exports.postUser = async(req,res) => {
     const { username,tel,password} = req.body 
     const newUser =new  User(req.body)
     await newUser.save()
+    message = `Hi, ${username} you have been registered successfully.`
+    await sendSms(message,tel)
     res.status(201).json({
         success:true,
         data:newUser
---
+++ utils/send_sms.js
@@ -0,0 +1,24 @@
+
+exports.sendSms = async (message,tel) =>{
+    console.log("sending")
+    console.log(message,tel)
+    const credentials = {
+        apiKey:process.env.apiKey,
+        username:process.env.username
+    }
+    const AfricasTalking = require('africastalking')(credentials)
+    const sms = AfricasTalking.SMS;
+        const options = {
+            to: tel,
+            message:message,
+            from:'3050'
+        }
+        console.log(options)
+        await sms.send(options)
+            .then( response => {
+                console.log(response);
+            })
+            .catch( error => {
+                console.log(error);
+            });
+}
